0c55b819b01d70ed426f22cdd7787aa94e710d7d
santiago-cortes <santiagocortes202@gmail.com>
2025-06-19 13:47:51 -0500

    mutabilidad y seguridad

---
+++ modules/src/main/java/com/platzi/functional/inmutable/mutable/MutablePerson_2.java
@@ -0,0 +1,51 @@
+package com.platzi.functional.inmutable.mutable;
+
+
+import java.util.List;
+
+/**
+ * Clase mejorada.
+ * Ahora obligamos a quien use esta clase a crear instancias usando el constructor.
+ * Quitamos el setter para evitar que hagan modificaciones peligrosas…
+ * sigue siendo mutable
+ */
+public class MutablePerson_2 {
+    private String firstName;
+    private String lastName;
+
+    private List<String> emails;
+
+    public MutablePerson_2(List<String> emails) {
+        this.emails = emails;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    @Override
+    public String toString() {
+        return "MutablePerson_2{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emails=" + emails +
+                '}';
+    }
+}
+
